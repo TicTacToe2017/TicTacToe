29740c8069c2edb221c59d63633a99ae8448959f
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2016-02-29 10:10:14 +0100

    clean-up and formatting

--- backend/routes/games.ts
+++ backend/routes/games.ts
@@ -17,7 +17,6 @@ games.post("/:name_player_x/:name_player_o", (req, res) => {
         res.status(403)
             .send("Game already exists");
     } else {
-        
         startedGames.push(new Game(player_x, player_o));
         res.sendStatus(201);
     }
@@ -27,36 +26,28 @@ games.post("/:name_player_x/:name_player_o", (req, res) => {
  * Get a game's grid between two players
  */
 games.get("/:name_player_x/:name_player_o", (req, res) => {
-    
     const player_x: string = req.params.name_player_x;
     const player_o: string = req.params.name_player_o;
-    const game : Game = startedGames.filter(g => g.player_x === player_x && g.player_o === player_o)[0];
-    
-    if (game) {        
+    const game: Game = startedGames.filter(g => g.player_x === player_x && g.player_o === player_o)[0];
+
+    if (game) {
         res.status(200)
-          .json(game.tiles)
+            .json(game.tiles);
     } else {
         res.status(404)
-            .send("Game does not exists");        
+            .send("Game does not exists");
     }
 });
 
 /**
- * Get all the games for a user
+ * Get all the games for an user
  */
-games.get("/:name_player", (req, res) => {
-    
-    const player: string = req.params.name_player;
-    
-    const games : Game[] = startedGames.filter(g => g.player_x === player || g.player_o === player);
-    
-    if (games) {
-         res.status(200)
-          res.send(games);
-    } else {
-          //res.status(404)
-          //  .send("User does not exist");   
-    }
+games.get("/:name", (req, res) => {
+    const name: string = req.params.name;
+
+    const games: Game[] = startedGames.filter(g => g.player_x === name || g.player_o === name);
+
+    res.send(games);
 });
 
 /**
@@ -65,23 +56,25 @@ games.get("/:name_player", (req, res) => {
 games.put("/:name_player_x/:name_player_o?tile=tile", (req, res) => {
     const player_x: string = req.params.name_player_x;
     const player_o: string = req.params.name_player_o;
-    const tileNumber = req.params.tile
+    const tileNumber = req.params.tile;
 
-    const game : Game = startedGames.some((g => g.player_x === player_x && g.player_o === player_o) || (g => g.player_o === player_x && g.player_x === player_o))[0];
+    const game: Game = startedGames.some(g =>
+        g.player_x === player_x && g.player_o === player_o
+        || g.player_o === player_x && g.player_x === player_o
+    )[0];
 
     if (game) {
         if(game.tiles[tileNumber] === Tile.null){
             (game.player_x === player_x) ? game.tiles[tileNumber] = Tile.x : game.tiles[tileNumber] = Tile.o;
             //TODO. Checks if player has won
+                res.status(403)
+                    .send("Tile is already marked");
+            }
         } else {
-            res.status(402)
-                .send("Tile is already taken");
-        }           
     } else {
         res.status(404)
             .send("Game does not exists");
     }
 });
-
 
 export default games;
