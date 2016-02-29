f5b82901c97f0324a80c576ce026777e8c06892e
jorgegzlezprez <jorgegzlezprez@gmail.com>
2016-02-29 20:04:13 +0100

    Test for move method

--- backend/spec/games.spec.ts
+++ backend/spec/games.spec.ts
@@ -28,3 +28,44 @@ describe("GET /games/name_player_x/name_player_o", () => {
         });
     });
 });
+
+describe("PUT /games/name_player_x/name_player_o?tile=tile", () => {
+    
+    it("Make a movement in an unexisted game", (done) => {
+        ApiClient.move("Spiderpig", "Batman",0, (err, res) => {
+            expect(err).toBeTruthy();
+            expect(err).toEqual("Game does not exists");
+        });
+    });
+    
+    it("Make first movement", (done) => {
+        ApiClient.move("Spiderman", "Batman",0, (err, res) => {
+            expect(err).toBeFalsy();            
+            done();
+        });
+    });
+    
+    it("Make second movement", (done) => {
+        ApiClient.move("Spiderman", "Batman",1, (err, res) => {
+            expect(err).toBeFalsy();            
+            done();
+        });
+    });
+    
+    it("Repeat the movement", (done) => {
+        ApiClient.move("Spiderman", "Batman",1, (err, res) => {
+            expect(err).toBeTruthy();
+            expect(err).toEqual("Tile is already marked");            
+            done();
+        });
+    });
+    
+    it("Do a winner movement", (done) => {
+        ApiClient.move("Spiderman", "Batman",2, (err, res) => {
+            expect(err).toBeFalsy();
+            expect(res).toEqual("You win!");
+            done();
+        });
+    });
+  
+});
