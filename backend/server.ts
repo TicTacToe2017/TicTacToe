a30c477903a3ea76aff6df16780ba4bf9d1e1dce
Jorge González Pérez <jorgegzlezprez@gmail.com>
2016-04-13 22:00:04 +0200

    Add access control allow methos

--- backend/server.ts
+++ backend/server.ts
@@ -13,9 +13,10 @@ let app: Express = express();
 app.use(bodyParser.json());
 
 app.use( function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
   next();
 });
 
