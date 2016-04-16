48ea6c0dfc42a8ef22c48d5ed481ddec0d99b6e9
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2016-04-16 16:48:39 +0200

    removed trailing whitespaces

--- backend/spec/games.spec.ts
+++ backend/spec/games.spec.ts
@@ -1,17 +1,17 @@
 import ApiClient from "./api-client";
 import Game from "../model/game";
 
-describe("GET /games/name", () => {    
+describe("GET /games/name", () => {
     it("lists all games which an user plays in", (done) => {
         ApiClient.getGamesForUser("Spiderman", (err, games: Game[]) => {
             expect(err).toBeFalsy();
             expect(games.length).toBe(0);
             done();
         });
-    });    
+    });
 });
 
-describe("GET /games/name_player_x/name_player_o", () => {    
+describe("GET /games/name_player_x/name_player_o", () => {
     it("creates a new game between two players if only it has not been created already", (done) => {
         ApiClient.startGame("Spiderman", "Batman", (err, res) => {
             expect(err).toBeFalsy();
@@ -26,10 +26,10 @@ describe("GET /games/name_player_x/name_player_o", () => {
             expect(err).toEqual("Game already exists");
             done();
         });
-    });   
+    });
 });
 
-describe("PUT /games/name_player_x/name_player_o?tile=tile", () => {    
+describe("PUT /games/name_player_x/name_player_o?tile=tile", () => {
     it("should return Game does not exists if making a movement in an inexisting game", (done) => {
         ApiClient.move("Spiderpig", "Batman", 0, (err, res) => {
             expect(res).toBeFalsy();
@@ -37,7 +37,7 @@ describe("PUT /games/name_player_x/name_player_o?tile=tile", () => {
             done();
         });
     });
-    
+
     it("should return a Tile marked status if making a movement in an existing game", (done) => {
         ApiClient.move("Spiderman", "Batman", 0, (err, res) => {
             expect(err).toBeFalsy();
@@ -45,15 +45,15 @@ describe("PUT /games/name_player_x/name_player_o?tile=tile", () => {
             done();
         });
     });
-    
+
     it("should return a Tile marked status if making a movement in an existing game", (done) => {
         ApiClient.move("Spiderman", "Batman", 3, (err, res) => {
             expect(err).toBeFalsy();
             expect(res).toEqual("Tile marked");
             done();
         });
-    });    
-    
+    });
+
     it("should return an error if moving in a marked tile", (done) => {
         ApiClient.move("Spiderman", "Batman", 0, (err, res) => {
             expect(res).toBeFalsy();
